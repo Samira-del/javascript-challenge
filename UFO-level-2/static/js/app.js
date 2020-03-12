cddf272fb1b8175e5ab09e9813e683449f05ed84
Samira-del <sa_mohamadzadeh@yahoo.com>
2020-03-12 16:21:46 -0700

    Upload Homework

---
+++ UFO-level-2/static/js/app.js
@@ -0,0 +1,65 @@
+// from data.js
+var tableData = data;
+
+var button = d3.select("#filter-btn");
+
+function createtable(UFOdata) {
+    var tbody = d3.select("tbody");
+    UFOdata.forEach((eachRecord) => {
+        var eachRow = tbody.append("tr");
+        Object.entries(eachRecord).forEach(([key, value]) => {
+            var eachCell = eachRow.append("td");
+            eachCell.html(value);
+        });
+    });
+};
+    
+// clear the table for new data
+function deleteTbody() {
+    d3.select("tbody")
+      .selectAll("tr").remove()
+      .selectAll("td").remove();
+  };
+  
+  // initial display of all UFO sightings
+  console.log(tableData);
+  createtable(tableData);
+  
+  // 'Filter Table' button
+  var button = d3.select("#filter-btn");
+  
+  // filter the database and display
+  button.on("click", function(event) {
+    d3.event.preventDefault();
+    deleteTbody();
+
+    var filteredData = tableData;
+    var inputId = document.getElementsByClassName("form-control");
+    
+     // iterate through all the input fields
+    for (var i = 0; i < inputId.length; i++) {
+	
+        var idName = inputId[i].id;
+        var field = d3.select("#" + idName).property("value");
+        
+        // treat empty or space-only fields as a search for ALL for that field
+        if (field.trim() !== "") {
+            var filteredData = filteredData.filter(ufoSighting =>
+                // match as case insensitive
+                ufoSighting[idName].toUpperCase().trim() ===
+                field.toUpperCase().trim());
+	    };
+    };
+  
+    // display message if no records found
+    if (filteredData.length == 0) {
+      d3.select("tbody")
+        .append("tr")
+        .append("td")
+        .attr("colspan", 7)
+        .html("<h4>No Records Found</h4>");
+    };
+  
+    console.log(filteredData);
+    createtable(filteredData);
+  });
